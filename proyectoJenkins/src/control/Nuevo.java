38aa108e18dd3364188a53af5a45735c24084df8
Noemi93 <noemdlm@hotmail.com>
2021-07-05 10:40:00 +0200

    prueba enviar correo

---
+++ proyectoJenkins/src/control/Nuevo.java
@@ -0,0 +1,13 @@
+package control;
+
+public class Nuevo {
+
+	public static void main(String[] args) {
+		 System.out.println("Hola Mundo Jenkins");
+		 System.out.println("Si lo veo es que no hay fallo");
+		 System.out.println("Prueba conexion rama noemiJenkins");
+		 System.out.println("Prueba2 conexion rama noemiJenkins");
+		 System.out.println("Modificacion por correo");
+
+		}
+}
